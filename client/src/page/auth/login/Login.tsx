34d3b4f71a0841b180b1df24f464e3be5abe60a4
omkarcode11 <omkarsonawaneomkar2@gmail.com>
2024-10-16 12:36:31 +0530

    signup and login ui done

---
+++ client/src/page/auth/login/Login.tsx
@@ -0,0 +1,37 @@
+import classes from "./Login.module.css";
+import { Link } from "react-router-dom";
+
+type Props = {};
+
+function Login({}: Props) {
+  return (
+    <div className={classes.container}>
+      <h1>Sign in to your account</h1>
+      <form className={classes.form}>
+        <div>
+          <label>Email address</label>
+          <input name="email" type="email" required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" required />
+        </div>
+        <div>
+          <label>I am a </label>
+          <select name="type">
+            <option value={"brand"}>Brand</option>
+            <option value={"creator"}>Creator</option>
+          </select>
+        </div>
+        <div>
+          <button className={classes.button} type="submit">Sign in</button>
+        </div>
+        <div>
+          <Link to={"/signup"}>Need an Account ? Register </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/page/auth/signup/Signup.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+import classes from './Signup.module.css'
+type Props = {};
+
+function Signup({}: Props) {
+  return (
+    <div className={classes.container}>
+      <h1>Create a new Account</h1>
+      <form className={classes.form}>
+        <div>
+          <label>Name</label>
+          <input name="text" type="name" required />
+        </div>
+        <div>
+          <label>Email address</label>
+          <input name="email" type="email" required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input name="password" type="password" required />
+        </div>
+        <div>
+          <label>Confirm Password</label>
+          <input name="password" type="password"  required />
+        </div>
+        <div>
+          <label>I am a </label>
+          <select name="type">
+            <option value={"brand"}>Brand</option>
+            <option value={"creator"}>Creator</option>
+          </select>
+        </div>
+        <div>
+          <button className={classes.button} type="submit">Sign up</button>
+        </div>
+        <div>
+          <Link to={"/"}>Already have an Account ? Sign in </Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
